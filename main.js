09e042a3a155971301ad5b7dcfb0abdde602b4a8
James <boydston.j@gmail.com>
2017-07-20 16:41:51 -0700

    check this out

--- main.js
+++ main.js
@@ -1,9 +1,72 @@
-/*
-  Here is a rough idea for the steps you could take:
-*/
+//#RecipeSearchDailyProject
 
-// 1. First select and store the elements you'll be working with
-// 2. Create your `submit` event for getting the user's search term
-// 3. Create your `fetch` request that is called after a submission
-// 4. Create a way to append the fetch results to your page
-// 5. Create a way to listen for a click that will play the song in the audio play
+let title = document.querySelector('title');
+let heading = document.querySelector('#heading');
+let main = document.querySelector('#main');
+let footing = document.querySelector('#footing');
+let recipeI = document.querySelector('#recipe');
+let searchI = document.getElementById('search');
+let pic= document.getElementsByClassName('pic')
+let audio = document.querySelector('.player');
+
+const site = "https://itunes.apple.com/search?term="
+let recipe = " "
+let ingred = " "
+let url = site + "hits"
+
+searchI.addEventListener ("click", function(event) {
+  recipe = recipeI.value
+  url = site + recipe
+  doFetch();
+})
+
+function doFetch() {
+  fetch(url)
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log(response.status);
+          return;
+        }
+        response.json().then(function(data) {
+          let titleStr = `${data.title} Search`
+
+            let audioStr = ``
+          // <section class="player">
+          //   <audio class="music-player" controls="controls" src=""></audio>
+          //   <hr>
+          // </section>
+
+            let mainStr = ``
+          data.results.map(function(item) {
+              mainStr += `<div class="boxes">`
+              mainStr += `<div class="pic"><img class="profilePic" src="${item.artworkUrl100}"></div>`
+              mainStr += `<audio class="music-player" controls="controls" src="${item.previewUrl}"></audio>`
+              mainStr += `<p class="rName"><a href="${item.trackViewUrl}">${item.trackName}</a><br><a href="${item.artistViewUrl}">${item.artistName}</a><br>
+              <a href="${item.collectionViewUrl}">${item.collectionName}</a></p>`
+              mainStr += `</div>`
+              main.innerHTML = mainStr
+          })
+
+
+          let footingStr = `<hr><footer>${data.title} Version ${data.version} <a href="${data.href}">${data.href}</a></footer>`
+          footing.innerHTML = footingStr
+        });
+      }
+    )
+    .catch(function(err) {
+      console.log("Fetch Error :-S", err);
+    });
+}
+
+searchI.addEventListener ("click", function(event) {
+  recipe = recipeI.value
+  url = site + recipe
+  doFetch();
+})
+
+doFetch()
+
+if (pic.innerHTML === '<img class="profilePic" src="">') {
+  console.log('what?');
+}
